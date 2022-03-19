71ae14a1f98fbed3949477870e6e6f9ae962b1a2
Manimaran <manimaranbhuwaneshwaran@gmail.com>
2022-03-20 00:18:38 +0530

    updates with acedemic and practice project details

--- js/scripts.js
+++ js/scripts.js
@@ -57,3 +57,11 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
+//Get the button
+let mybutton = document.getElementById("btn-back-to-top");
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
