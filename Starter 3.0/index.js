09710c3cdedec0f3d04e2dbc15622a522eb3a4b6
Manimaran-Bhuwaneshwaran <62472418+Manimaran-Bhuwaneshwaran@users.noreply.github.com>
2025-06-01 00:30:02 +0530

    cm agri 3.0

---
+++ Starter 3.0/index.js
@@ -0,0 +1,194 @@
+// const chemical_fertilizer= [{"name":"Zuari Fertilizers Jai Kisaan Samrat","source":"https://images.jdmagicbox.com/quickquotes/images_main/zuari-fertilizers-jai-kisaan-samrat-219910491-m62a1.png"},{"name":"Zuari Fertilizers Jai Kisaan Suraksha","source":"https://images.jdmagicbox.com/quickquotes/images_main/zuari-fertilizers-jai-kisaan-suraksha-219910494-o2ayv.png"},{"name":"Zuari Speciality Fertilizers Sanjeevani","source":"https://images.jdmagicbox.com/quickquotes/images_main/zuari-speciality-fertilizers-sanjeevani-219910500-lk0dl.png"},{"name":"Zuari Speciality Fertilizers Sulpho 50","source":"https://images.jdmagicbox.com/quickquotes/images_main/zuari-speciality-fertilizers-sulpho-50-219910502-lbb9n.png"},{"name":"Zuari Speciality Fertilizers Atom 61","source":"https://images.jdmagicbox.com/quickquotes/images_main/zuari-speciality-fertilizers-atom-61-219910495-au6y8.png"},{"name":"Zuari Speciality Fertilizers Calnit 19","source":"https://images.jdmagicbox.com/quickquotes/images_main/zuari-speciality-fertilizers-calnit-19-219910498-msp3a.png"},{"name":"Zuari Speciality Fertilizers Poorna 19","source":"https://images.jdmagicbox.com/quickquotes/images_main/zuari-speciality-fertilizers-poorna-19-219910499-rcd8x.png"},{"name":"Zuari Fertilizers Jai Kisaan Samarth","source":"https://images.jdmagicbox.com/quickquotes/images_main/zuari-fertilizers-jai-kisaan-samarth-219910488-c34vb.png"},{"name":"Zuari Speciality Fertilizers Boost 52","source":"https://images.jdmagicbox.com/quickquotes/images_main/zuari-speciality-fertilizers-boost-52-219910497-nnc23.png"},{"name":"Zuari Organic Manure JK Compost","source":"https://images.jdmagicbox.com/quickquotes/images_main/zuari-organic-manure-jk-compost-219910503-1rzvn.png"},{"name":"Zuari Fertilizers Jai Kisaan SOP","source":"https://images.jdmagicbox.com/quickquotes/images_main/zuari-fertilizers-jai-kisaan-sop-219910492-f3kii.png"},{"name":"Zuari Speciality Fertilizers Boon 45","source":"https://images.jdmagicbox.com/quickquotes/images_main/zuari-speciality-fertilizers-boon-45-219910496-o7ome.png"},{"name":"Zuari Fertilizers Jai Kisaan Sampatti","source":"https://images.jdmagicbox.com/quickquotes/images_main/zuari-fertilizers-jai-kisaan-sampatti-219910489-u6dia.png"},{"name":"Zuari Fertilizers Jai Kisaan Urea","source":"https://images.jdmagicbox.com/quickquotes/images_main/zuari-fertilizers-jai-kisaan-urea-219910493-6vomv.png"},{"name":"Zuari Speciality Fertilizers Satej","source":"https://images.jdmagicbox.com/quickquotes/images_main/zuari-speciality-fertilizers-satej-219910501-xmdzm.png"},{"name":"Zuari Fertilizers Jai Kisaan Sampurna","source":"https://images.jdmagicbox.com/quickquotes/images_main/zuari-fertilizers-jai-kisaan-sampurna-219910490-6mtni.png"}];
+
+// var inst = document.getElementsByClassName("body")[0];
+// chemical_fertilizer.forEach(el=>{
+// 	inst.innerHTML += '<div class="body-el"><div class="special-discount"><p>200 off</p> </div> <div class="sold-out"> <p>sold</p> </div> <div class="image"> <img src="'+el.source+'" alt="'+el.name+'"> </div> <div class="content"> <p>'+el.name+'</p> <p class="prize">Rs.250 <small>Rs.450</small></p> </div> <div class="button-group"> <button>Add to Cart</button> </div> </div>';
+
+// const bio_fertilizer = [{"name":"Surya Z-Mob Zinc Mobilising Biofertilizer","source":"https://images.jdmagicbox.com/quickquotes/images_main/surya-fertilizers-and-soil-additives-24-12-2020-007-219949781-8f5qu.jpg"},{"name":"Surya Vam Biofertilizer","source":"https://images.jdmagicbox.com/quickquotes/images_main/surya-fertilizers-and-soil-additives-24-12-2020-011-219949785-k6yda.jpg"},{"name":"Surya N-Fix - Glucanobacter","source":"https://images.jdmagicbox.com/quickquotes/images_main/surya-fertilizers-and-soil-additives-24-12-2020-004-219949778-yn4zy.jpg"},{"name":"Surya Mg-Mob Magnesium Mobilizing Biofertilizer","source":"https://images.jdmagicbox.com/quickquotes/images_main/surya-fertilizers-and-soil-additives-24-12-2020-008-219949782-kaeak.jpg"},{"name":"Surya S-Sol Sulphur Mobilising Biofertilizer","source":"https://images.jdmagicbox.com/quickquotes/images_main/surya-fertilizers-and-soil-additives-24-12-2020-009-219949783-3mm5x.jpg"},{"name":"Surya Fe- Sol Ferrous Mobilising Biofertilizer","source":"https://images.jdmagicbox.com/quickquotes/images_main/surya-fertilizers-and-soil-additives-24-12-2020-010-219949784-zjihm.jpg"},{"name":"Surya P-Sol Phosphate Solubilizing Biofertilizer","source":"https://images.jdmagicbox.com/quickquotes/images_main/surya-fertilizers-and-soil-additives-24-12-2020-005-219949779-jtxtj.jpg"},{"name":"Surya K-Mob Potash Mobilizing Biofertilizer","source":"https://images.jdmagicbox.com/quickquotes/images_main/surya-fertilizers-and-soil-additives-24-12-2020-006-219949780-l8hpa.jpg"}];
+
+// const bio_pesticide=[{"name":"Surya Treat Bio Pesticide","source":"https://images.jdmagicbox.com/quickquotes/images_main/surya-agricultural-pesticides-24-12-2020-012-219949786-iugf8.jpg"},{"name":"Surya Pmdc (F5) Bio Pesticide","source":"https://images.jdmagicbox.com/quickquotes/images_main/surya-agricultural-pesticides-24-12-2020-016-219949790-2q4p3.jpg"},{"name":"Surya Natural Lp Bio Pesticide","source":"https://images.jdmagicbox.com/quickquotes/images_main/surya-agricultural-pesticides-24-12-2020-017-219949791-clj53.jpg"},{"name":"Surya Natural Tm Bio Pesticide","source":"https://images.jdmagicbox.com/quickquotes/images_main/surya-agricultural-pesticides-24-12-2020-018-219949792-1owzi.jpg"},{"name":"Surya Pmdc (F11) Bio Pesticide","source":"https://images.jdmagicbox.com/quickquotes/images_main/surya-agricultural-pesticides-24-12-2020-019-219949793-cnygm.jpg"},{"name":"Surya B.T Killer Bio Pesticide","source":"https://images.jdmagicbox.com/quickquotes/images_main/surya-agricultural-pesticides-24-12-2020-015-219949789-2xoml.jpg"},{"name":"Surya Treat T.H Bio Pesticide","source":"https://images.jdmagicbox.com/quickquotes/images_main/surya-agricultural-pesticides-24-12-2020-013-219949787-ayykl.jpg"},{"name":"Surya Spot Bio Pesticide","source":"https://images.jdmagicbox.com/quickquotes/images_main/surya-agricultural-pesticides-24-12-2020-014-219949788-dbp4s.jpg"}];
+
+// const agricultural_fertilizer = [{"name":"Surya Bio-Mix","source":"https://images.jdmagicbox.com/quickquotes/images_main/surya-fertilizers-and-soil-additives-24-12-2020-024-219949798-0tavr.jpg"},{"name":"Surya Hale Bio Stimulators","source":"https://images.jdmagicbox.com/quickquotes/images_main/surya-fertilizers-and-soil-additives-24-12-2020-020-219949794-fazob.jpg"},{"name":"Surya S-88 Bio Stimulators","source":"https://images.jdmagicbox.com/quickquotes/images_main/surya-fertilizers-and-soil-additives-24-12-2020-022-219949796-x1xbb.jpg"},{"name":"Surya Arrow Bio Stimulators","source":"https://images.jdmagicbox.com/quickquotes/images_main/surya-fertilizers-and-soil-additives-24-12-2020-021-219949795-gkqx9.jpg"}];
+
+const allProducts = {"Zuari Organic Manure JK Compost": "zuari-organic-manure-jk-compost-219910503-1rzvn.png",
+"Zuari Speciality Fertilizers Sanjeevani": "zuari-speciality-fertilizers-sanjeevani-219910500-lk0dl.png",
+"Zuari Speciality Fertilizers Sulpho 50": "zuari-speciality-fertilizers-sulpho-50-219910502-lbb9n.png",
+"Zuari Speciality Fertilizers Calnit 19": "zuari-speciality-fertilizers-calnit-19-219910498-msp3a.png",
+"Surya N-Fix - Glucanobacter": "surya-fertilizers-and-soil-additives-24-12-2020-004-219949778-yn4zy.jpg",
+"Surya Z-Mob Zinc Mobilising Biofertilizer": "surya-fertilizers-and-soil-additives-24-12-2020-007-219949781-8f5qu.jpg",
+"Surya Vam Biofertilizer": "surya-fertilizers-and-soil-additives-24-12-2020-011-219949785-k6yda.jpg",
+"Surya Mg-Mob Magnesium Mobilizing Biofertilizer": "surya-fertilizers-and-soil-additives-24-12-2020-008-219949782-kaeak.jpg",
+"Surya Treat Bio Pesticide": "surya-agricultural-pesticides-24-12-2020-012-219949786-iugf8.jpg",
+"Surya Spot Bio Pesticide": "surya-agricultural-pesticides-24-12-2020-014-219949788-dbp4s.jpg",
+"Surya Pmdc (F5) Bio Pesticide": "surya-agricultural-pesticides-24-12-2020-016-219949790-2q4p3.jpg",
+"Surya Natural Lp Bio Pesticide": "surya-agricultural-pesticides-24-12-2020-017-219949791-clj53.jpg",
+"Surya Hale Bio Stimulators": "surya-fertilizers-and-soil-additives-24-12-2020-020-219949794-fazob.jpg",
+"Surya S-88 Bio Stimulators": "surya-fertilizers-and-soil-additives-24-12-2020-022-219949796-x1xbb.jpg",
+"Surya Arrow Bio Stimulators": "surya-fertilizers-and-soil-additives-24-12-2020-021-219949795-gkqx9.jpg",
+"Surya Bio-Mix": "surya-fertilizers-and-soil-additives-24-12-2020-024-219949798-0tavr.jpg",
+"Surya N-Fix - Azospirillum": "surya-fertilizers-and-soil-additives-24-12-2020-002-219949776-kyt2n.jpg",
+"Surya N-Fix - Azotobacter": "surya-fertilizers-and-soil-additives-24-12-2020-001-219949775-hqthi.jpg",
+"Surya Gold Organic Manure": "surya-fertilizers-and-soil-additives-24-12-2020-023-219949797-oatid.jpg",
+"Surya N-Fix - Rhizobium": "surya-fertilizers-and-soil-additives-24-12-2020-003-219949777-meqo6.jpg"}
+
+const chemical_fretilizers = {"Zuari Fertilizers Jai Kisaan Samrat": "Chemical Fertilizer/zuari-fertilizers-jai-kisaan-samrat-219910491-m62a1.jpg",
+"Zuari Fertilizers Jai Kisaan Suraksha": "Chemical Fertilizer/zuari-fertilizers-jai-kisaan-suraksha-219910494-o2ayv.avif",
+"Zuari Speciality Fertilizers Sanjeevani": "Chemical Fertilizer/zuari-speciality-fertilizers-sanjeevani-219910500-lk0dl.avif",
+"Zuari Speciality Fertilizers Sulpho 50": "Chemical Fertilizer/zuari-speciality-fertilizers-sulpho-50-219910502-lbb9n.avif",
+"Zuari Speciality Fertilizers Atom 61": "Chemical Fertilizer/zuari-speciality-fertilizers-atom-61-219910495-au6y8.avif",
+"Zuari Speciality Fertilizers Calnit 19": "Chemical Fertilizer/zuari-speciality-fertilizers-calnit-19-219910498-msp3a.avif",
+"Zuari Speciality Fertilizers Poorna 19": "Chemical Fertilizer/zuari-speciality-fertilizers-poorna-19-219910499-rcd8x.avif",
+"Zuari Fertilizers Jai Kisaan Samarth": "Chemical Fertilizer/zuari-fertilizers-jai-kisaan-samarth-219910488-c34vb.avif",
+"Zuari Speciality Fertilizers Boost 52": "Chemical Fertilizer/zuari-speciality-fertilizers-boost-52-219910497-nnc23.avif",
+"Zuari Organic Manure JK Compost": "Chemical Fertilizer/zuari-organic-manure-jk-compost-219910503-1rzvn.avif",
+"Zuari Fertilizers Jai Kisaan SOP": "Chemical Fertilizer/zuari-fertilizers-jai-kisaan-sop-219910492-f3kii.avif",
+"Zuari Speciality Fertilizers Boon 45": "Chemical Fertilizer/zuari-speciality-fertilizers-boon-45-219910496-o7ome.avif",
+"Zuari Fertilizers Jai Kisaan Sampatti": "Chemical Fertilizer/zuari-fertilizers-jai-kisaan-sampatti-219910489-u6dia.avif",
+"Zuari Fertilizers Jai Kisaan Urea": "Chemical Fertilizer/zuari-fertilizers-jai-kisaan-urea-219910493-6vomv.jpg",
+"Zuari Speciality Fertilizers Satej": "Chemical Fertilizer/zuari-speciality-fertilizers-satej-219910501-xmdzm.avif",
+"Zuari Fertilizers Jai Kisaan Sampurna": "Chemical Fertilizer/zuari-fertilizers-jai-kisaan-sampurna-219910490-6mtni.avif"}
+
+const bio_fertilizer = {"Surya N-Fix - Glucanobacter": "Bio Fertilizer/surya-fertilizers-and-soil-additives-24-12-2020-004-219949778-yn4zy.webp",
+"Surya Z-Mob Zinc Mobilising Biofertilizer": "Bio Fertilizer/surya-fertilizers-and-soil-additives-24-12-2020-007-219949781-8f5qu.jpg",
+"Surya Vam Biofertilizer": "Bio Fertilizer/surya-fertilizers-and-soil-additives-24-12-2020-011-219949785-k6yda.avif",
+"Surya Mg-Mob Magnesium Mobilizing Biofertilizer": "Bio Fertilizer/surya-fertilizers-and-soil-additives-24-12-2020-008-219949782-kaeak.jpg",
+"Surya S-Sol Sulphur Mobilising Biofertilizer": "Bio Fertilizer/surya-fertilizers-and-soil-additives-24-12-2020-009-219949783-3mm5x.jpg",
+"Surya Fe- Sol Ferrous Mobilising Biofertilizer": "Bio Fertilizer/surya-fertilizers-and-soil-additives-24-12-2020-010-219949784-zjihm.jpg",
+"Surya P-Sol Phosphate Solubilizing Biofertilizer": "Bio Fertilizer/surya-fertilizers-and-soil-additives-24-12-2020-005-219949779-jtxtj.jpg",
+"Surya K-Mob Potash Mobilizing Biofertilizer": "Bio Fertilizer/surya-fertilizers-and-soil-additives-24-12-2020-006-219949780-l8hpa.jpg"}
+
+const bio_pesticide = {"Surya Treat Bio Pesticide": "Bio Pesticides/surya-agricultural-pesticides-24-12-2020-012-219949786-iugf8.avif",
+"Surya Pmdc (F5) Bio Pesticide": "Bio Pesticides/surya-agricultural-pesticides-24-12-2020-016-219949790-2q4p3.webp",
+"Surya Natural Lp Bio Pesticide": "Bio Pesticides/surya-agricultural-pesticides-24-12-2020-017-219949791-clj53.jpg",
+"Surya Natural Tm Bio Pesticide": "Bio Pesticides/surya-agricultural-pesticides-24-12-2020-018-219949792-1owzi.jpg",
+"Surya Pmdc (F11) Bio Pesticide": "Bio Pesticides/surya-agricultural-pesticides-24-12-2020-019-219949793-cnygm.jpg",
+"Surya B.T Killer Bio Pesticide": "Bio Pesticides/surya-agricultural-pesticides-24-12-2020-015-219949789-2xoml.avif",
+"Surya Treat T.H Bio Pesticide": "Bio Pesticides/surya-agricultural-pesticides-24-12-2020-013-219949787-ayykl.webp",
+"Surya Spot Bio Pesticide": "Bio Pesticides/surya-agricultural-pesticides-24-12-2020-014-219949788-dbp4s.webp"}
+
+const agricultural_fertilizer = {"Surya S-88 Bio Stimulators": "Agricultural Fertilizer/surya-fertilizers-and-soil-additives-24-12-2020-022-219949796-x1xbb.jpg",
+"Surya Hale Bio Stimulators": "Agricultural Fertilizer/surya-fertilizers-and-soil-additives-24-12-2020-020-219949794-fazob.jpg",
+"Surya Bio-Mix": "Agricultural Fertilizer/surya-fertilizers-and-soil-additives-24-12-2020-024-219949798-0tavr.jpg",
+"Surya Arrow Bio Stimulators": "Agricultural Fertilizer/surya-fertilizers-and-soil-additives-24-12-2020-021-219949795-gkqx9.jpg"}
+
+const azospirillum = {"Surya N-Fix - Azospirillum": "Azospirillum/surya-fertilizers-and-soil-additives-24-12-2020-002-219949776-kyt2n.jpg"}
+
+const azotobacter = {"Surya N-Fix - Azotobacter": "Azotobacter/surya-fertilizers-and-soil-additives-24-12-2020-001-219949775-hqthi.jpg"}
+
+const organic_manure = {"Surya Gold Organic Manure": "Organic Manure/surya-fertilizers-and-soil-additives-24-12-2020-023-219949797-oatid.jpg"}
+
+const rhizobium = {"Surya N-Fix - Rhizobium": "Rhizobium/surya-fertilizers-and-soil-additives-24-12-2020-003-219949777-meqo6.jpg"}
+
+const categories = ["Rhizobium Biofertilizer",
+"Organic Manure",
+"Azotobacter Biofertilizer",
+"Azospirillum Biofertilizer",
+"Agricultural Fertilizer",
+"Biopesticide",
+"Bio Fertilizer",
+"Chemical Fertilizer"]
+
+const categories_data_map = {"Rhizobium Biofertilizer": rhizobium,
+"Organic Manure": organic_manure,
+"Azotobacter Biofertilizer": azotobacter,
+"Azospirillum Biofertilizer": azospirillum,
+"Agricultural Fertilizer": agricultural_fertilizer,
+"Biopesticide": bio_pesticide,
+"Bio Fertilizer": bio_fertilizer,
+"Chemical Fertilizer": chemical_fretilizers}
+
+const renderView = document.getElementsByClassName("product-elements")[0];
+let bc = 0;
+for (const [key, value] of Object.entries(allProducts)) {
+	if(bc == 4) {
+		break;
+	}
+	renderView.innerHTML+=`<div class="body-el body-el-g1"  onclick="window.open('./product/index.html', '_self')">
+	<div class="image" style="background-image:url('./assets/img/products/${value}')"> </div>
+	<div class="content">
+		<p>${key}</p>
+	</div>
+</div>`;
+bc++;
+  }
+
+
+const root = document.getElementById("root");
+const root2 = document.getElementById("shopy-by-cat");
+const catagoriesRoot = document.getElementsByClassName("catagories")[0];
+function closeRoot() {
+	catagoriesRoot.style.animation = "slideOutAnimation ease 1s"
+	setTimeout(() => {
+		catagoriesRoot.style.display = "none";
+	}, 1000);
+}
+function showRoot() {
+	catagoriesRoot.style.animation = "slideInAnimation ease 1s"
+	catagoriesRoot.style.display = "block";
+}
+catagoriesRoot.style.display = "none";
+
+const renderViewNa = document.getElementsByClassName("product-elements")[1];
+for (const [key, value] of Object.entries(categories_data_map)) {
+	root.innerHTML += `<div class="chead"><li>${key} <i class="fa fa-caret-down"></i></li></div>`;
+	var outString = ``;
+	outString += `<div class="sbhead"><li>${key} <i class="fa fa-caret-down"></i></li><ul>`;
+	var visited = false;
+	for (const [ikey, ivalue] of Object.entries(value)) {
+		if( visited != true) {
+			renderViewNa.innerHTML+=`<div class="body-el body-el-g2" onclick="window.open('./product/index.html', '_self')">
+				<div class="image" style="background-image:url('./assets/img/products/${ivalue}')"> </div>
+				<div class="content">
+					<p>${ikey}</p>
+				</div>
+			</div>`;
+		}
+		root.innerHTML += `<div class="cbody"><li><i class="fa fa-caret-right"></i> ${ikey}</li>
+		<img alt="${ikey}"></div>`;
+		
+		outString += `<div class="sbbody"><li><i class="fa fa-caret-right"></i> ${ikey}</li>
+		<img alt="${ikey}" src='./assets/img/products/${ivalue}'  src='./assets/img/products/${ivalue}'  onclick="window.open('./product/index.html', '_self')" style="cursor: pointer"></div>`;
+		visited = true;
+	  }
+	  outString += `</ul></div>`;
+	  root2.innerHTML += outString;
+  }
+
+const slide = document.getElementsByClassName("slide");
+const square = document.getElementsByClassName("square");
+var ss = 0;
+function showSlide() {
+	if(ss >= slide.length) {
+		ss = 0;
+	}
+	if(ss < 0) {
+		ss = slide.length-1;
+	}
+	for (let index = 0; index < slide.length; index++) {
+		
+		
+		slide[index].style.width = "0px";
+		slide[index].style.height = "0px";
+		square[index].className = "square fill fa fa-circle-o";
+		if(index == ss) {
+			slide[index].style.width = "100%";
+			slide[index].style.height = "auto";
+			square[index].className = "square fill fa fa-circle";
+		}
+	}
+}
+showSlide();
+setInterval(() => {
+	showSlide();
+	ss++;
+}, 3000);
+
+function slideBack() {
+	ss--;
+	showSlide();
+}
+
+function slideForward() {
+	ss++;
+	showSlide();
+}
